d06ce0b1380e36029ca656eed2646f72d42f1045
Senrathy <sergioricardort@hotmail.com>
2022-10-14 07:03:55 -0300

    Helpers and SplashPage started

--- lib/app/pages/splash/splash_page.dart
+++ lib/app/pages/splash/splash_page.dart
@@ -1,38 +1,34 @@
 import 'package:flutter/material.dart';
+import 'package:fwc_album_app/app/core/ui/helpers/loader.dart';
+import 'package:fwc_album_app/app/core/ui/helpers/messages.dart';
 import 'package:fwc_album_app/app/core/ui/styles/button_styles.dart';
 import 'package:fwc_album_app/app/core/ui/styles/colors_app.dart';
 import 'package:fwc_album_app/app/core/ui/styles/text_styles.dart';
 import 'package:fwc_album_app/app/core/ui/widgets/button.dart';
 import 'package:fwc_album_app/app/core/ui/widgets/rounded_button.dart';
 
-class SplashPage extends StatelessWidget {
+class SplashPage extends StatefulWidget {
   const SplashPage({Key? key}) : super(key: key);
 
   @override
+  State<SplashPage> createState() => _SplashPageState();
+}
+
+class _SplashPageState extends State<SplashPage>
+    with Loader<SplashPage>, Messages<SplashPage> {
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.white,
-      appBar: AppBar(
-        title: const Text('Splash Page'),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Button(
-              style: context.buttonStyles.primaryButton,
-              labelStyle: context.textStyles.textPrimaryFontBold,
-              label: 'Salvar',
-              onPressed: () {},
-            ),
-            Button.primary(
-              label: 'Coiso',
-              onPressed: () {},
-              width: MediaQuery.of(context).size.width * .9,
-            ),
-            RoundedButton(icon: Icons.add, onPressed: (){}),
-            const TextField(),
-          ],
+      backgroundColor: context.colors.primary,
+      body: DecoratedBox(
+        decoration: const BoxDecoration(
+          image: DecorationImage(
+            image: AssetImage('assets/images/background_splash.png'),
+            fit: BoxFit.cover
+          ),
+        ),
+        child: Stack(
+          children: [],
         ),
       ),
     );
